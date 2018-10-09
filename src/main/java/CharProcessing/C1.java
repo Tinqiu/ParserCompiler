b776fa0ced7b88910d591dfdbfc408ee03cedb95
Tinqiu <35305066+Tinqiu@users.noreply.github.com>
2018-10-09 16:11:32 -0400

    Semi-final implementation

--- src/main/java/CharProcessing/C1.java
+++ src/main/java/CharProcessing/C1.java
@@ -1,99 +1,183 @@
 package CharProcessing;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Pattern;
+
+import static CharProcessing.Type.*;
 
 
 enum Type{
-    ALPHA, NUM, SPACE, START
+    ALPHA, NUM, SPACE, PSTART, PEND, ALPHAX, NUMX
 }
 
 public class C1 {
     private List<String> patternForm = new ArrayList<>();
+    private List<String> transformOrder = new ArrayList<>();
+    private int patternLength = 1;
+    private HashMap<Type, Integer> matchingTokens  = new HashMap<>();
+    private HashMap<Type, Integer> transformTokens  = new HashMap<>();
 
-    public String createMatchignPattern(String s) {
-            s.chars()
-                    .mapToObj(c -> (char) c)
-                    .forEach(c -> preBuildPattern(c));
-        return compressString(patternForm, false);
+    public C1(){
+        for(Type t : Type.values()){
+            matchingTokens.put(t,0);
+            transformTokens.put(t,0);
+        }
     }
 
-    public String createTransformPattern(String s) {
+    public int getPatternWordCount(){return trailingSpace ? patternLength -1 : patternLength;}
+
+    public String createPatternForMatching(String s) {
+        resetInternals();
         s.chars()
                 .mapToObj(c -> (char) c)
-                .forEach(c -> preBuildPattern(c));
-        return compressString(patternForm, true);
+                .forEach(c -> tokenizePattern(c,false));
+        return compressAndConvert(patternForm, false);
     }
 
+    public List<String> createPatternForTransforming(String s){
+        resetInternals();
+        s.chars()
+                .mapToObj(c -> (char) c)
+                .forEach(c -> tokenizePattern(c,true));
+        patternForm.forEach(this::getMatchType);
+        return transformOrder;
+    }
 
-    static boolean perfectFlag = false;
-    private void preBuildPattern(Character c){
+    public Boolean transformAvailable(){
+        for(Type t : Type.values()){
+            if(transformTokens.get(t) > matchingTokens.get(t)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean perfectFlag = false;
+    private void tokenizePattern(Character c, Boolean transform){
         if(perfectFlag){
             if(c.compareTo('>')!=0){
                 patternForm.add(c.toString());
             }
-            else perfectFlag = false;
+            else {
+                patternForm.add(PEND.name());
+                perfectFlag = false;
+            }
         }
         else {
             if (c.compareTo('a') == 0) {
-                patternForm.add("ALPHA");
+                patternForm.add(ALPHA.name());
+                updateMap(ALPHA,transform);
+            } else if(c.compareTo('A') == 0){
+                patternForm.add(ALPHAX.name());
+                updateMap(ALPHAX,transform);
             } else if (c.compareTo('n') == 0) {
-                patternForm.add("NUM");
+                patternForm.add(NUM.name());
+                updateMap(NUM,transform);
+            } else if (c.compareTo('N') == 0) {
+                patternForm.add(NUMX.name());
+                updateMap(NUMX,transform);
             } else if (c.compareTo(' ') == 0) {
-                patternForm.add("SPACE");
+                patternForm.add(SPACE.name());
+                updateMap(SPACE,transform);
             } else if (c.compareTo('<') == 0) {
+                patternForm.add(PSTART.name());
                 perfectFlag = true;
             }
         }
     }
 
-    //convert and compress the string to a regex pattern with Run Length encoding
-    private String compressString(List<String> toCompress, boolean toTransform){
-        String compressed = "";
-
-        String str = toTransform ? "" : "^";
-        int count=1;
-        for (String val : toCompress) {
-            if(val.equals(str)){
-                if(toTransform){
-                    compressed = compressed + getMatchType(str) +",";
-                }else count++;
-            }
-            else {
-                if(toTransform){
-                    compressed = compressed + getMatchType(str) +",";
-                } else compressed = compressed + getMatchType(str);
-                if(count != 1){
-                    if(toTransform){
-                        compressed = compressed + getMatchType(str) +",";
-                    }else compressed = compressed + "{" + String.valueOf(count) +"}";
-                }
-                str = val;
-                count = 1;
-            }
-        }
-        if(toTransform){
-            compressed = compressed + getMatchType(str) +",";
-        } else compressed = compressed + getMatchType(str);
-        if(count != 1){
-            if(toTransform){
-                compressed = compressed + getMatchType(str) +",";
-            } else compressed = compressed + "{" + String.valueOf(count) +"}";
-            return toTransform ? compressed.substring(1,compressed.length()-1) : compressed + "$";
-        }
-        return toTransform ? compressed.substring(1,compressed.length()-1) : compressed + "$";
-
+    private void updateMap(Type type, Boolean transform){
+        HashMap<Type, Integer> tempMap = transform ? transformTokens : matchingTokens;
+        tempMap.put(type,tempMap.get(type)+1);
     }
+
+
+    private String compressAndConvert(List<String> toCompress, boolean toTransform){
+        StringBuilder converted = new StringBuilder(toTransform ? "" : "^");
+        toCompress
+                .forEach(c -> converted.append(toTransform ? getMatchType(c) + "," : getMatchType(c)));
+
+        String returnString = converted.toString();
+        //remove the trailing comma before returning if it was a transform pattern that was generated
+        return toTransform ? returnString.substring(1, returnString.length()-1) : returnString + "$";
+    }
+
+    private int aIndex = 0;
+    private int nIndex = 0;
+    private int pIndex = 0;
+    private int bigNIndex = 0;
+    private int bigAIndex = 0;
+    private int sIndex = 0;
+    private boolean trailingSpace = false;
+    private String tempPerfectMatch = "";
 
     private String getMatchType(String s){
+        String toAdd = "";
 
-        if(s.equals("ALPHA"))
-            return "\\p{L}";
-        else if(s.equals("NUM"))
-            return "\\p{N}";
-        else if(s.equals("SPACE"))
-            return  " ";
-        else return s;
+        if(s.equals(ALPHA.name())) {
+            trailingSpace = false;
+            aIndex ++;
+            toAdd = "a" + aIndex;
+            transformOrder.add(toAdd);
+            return "(?<" + toAdd + ">\\p{L})";
+        }
+        else if(s.equals(ALPHAX.name())) {
+            trailingSpace = false;
+            bigAIndex ++;
+            toAdd = "A" + bigAIndex;
+            transformOrder.add(toAdd);
+            return "(?<" + toAdd + ">\\p{L}*)";
+        }
+        else if(s.equals(NUM.name())){
+            trailingSpace = false;
+            nIndex++;
+            toAdd = "n" + nIndex;
+            transformOrder.add(toAdd);
+            return "(?<" + toAdd + ">\\p{N})";
+        }
+        else if(s.equals(NUMX.name())) {
+            trailingSpace = false;
+            bigNIndex ++;
+            toAdd = "N" + bigNIndex;
+            transformOrder.add(toAdd);
+            return "(?<" + toAdd + ">\\p{N}*)";
+        }
+        else if(s.equals(SPACE.name())) {
+            sIndex++;
+            patternLength = trailingSpace ? patternLength + 1 : patternLength + 2;
+            trailingSpace = true;
+            toAdd = "s" + sIndex;
+            transformOrder.add(toAdd);
+            return "(?<" + toAdd + "> )";
+        }
+        else if(s.equals(PSTART.name())){
+            trailingSpace = false;
+            pIndex++;
+            toAdd = "p" + pIndex;
+            tempPerfectMatch = "~";
+            return "(?<" + toAdd + ">";
+        }
+        else if(s.equals(PEND.name())){
+            transformOrder.add(tempPerfectMatch);
+            tempPerfectMatch = "";
+            return ")";
+        }
+        else{
+            tempPerfectMatch = tempPerfectMatch + s;
+            return s;
+        }
     }
 
+    private void resetInternals(){
+        aIndex = 0;
+        nIndex = 0;
+        pIndex = 0;
+        bigNIndex = 0;
+        bigAIndex = 0;
+        sIndex = 0;
+        patternForm = new ArrayList<>();
+        transformOrder = new ArrayList<>();
+    }
 }
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -7,97 +7,99 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
-    static List<String> compiledMatchingPatterns = new ArrayList<>();
-    static List<String> compiledTransformPatterns = new ArrayList<>();
+    private static final String SPLIT_ON_SPACE_LOOKBEHIND = "((?<=[\\s])|(?=[\\s]))";
+
     public static void main(String args[]){
-        String[] inPatterns = {"a1n1a2 n2a3n3,a1n1a2n2a3n3",
-                                "<code>   ,  <code>  "};
-        String[] toMatch = {"G1Q 1Q9",
-                            "Hi, this is my postal code   tw"};
+        ArrayList<String[]> inPatterns = new ArrayList<>();
 
-        for(String var : toMatch){
-            for(String inPattern : inPatterns) {
-                C1 c1 = new C1();
+        String toMatch = "G1Q  1Q9";
 
-                int patternCount = Arrays.asList(inPattern.split("((?<=\\s)|(?=\\s))")).size();
-
-                compiledMatchingPatterns.add(c1.createMatchignPattern(inPattern.split(",")[0]));
-                generateTransformPatterns(inPattern.split(",")[1]);
-
-                Pattern pattern = Pattern.compile(compiledMatchingPatterns.get(compiledMatchingPatterns.size() - 1));
-                process(pattern, var, patternCount);
-
-                //System.out.println(compiledPatterns.get(compiledPatterns.size()-1));
-            }
+        String[] patterns = {"a1n1a2  n2a3n3","<test>a1n1a2 n2a3n3"};
+        inPatterns.add(patterns);
+        try {
+            match(inPatterns, toMatch);
+        }catch (TransformPatternTooLongException ex){
+            ex.printStackTrace();
         }
-
     }
 
-    private static void generateTransformPatterns(String prePattern){
-        C1 c1 = new C1();
-        compiledTransformPatterns.add(c1.createTransformPattern(prePattern));
-    }
-
-
-
-    private static void process(Pattern compiledPattern, String toMatch, int patternCount){
-        List<String> reverseToMatch = new ArrayList();
-        Stream.of(toMatch.split("((?<=\\W)|(?=\\W))"))
+    private static void match(ArrayList<String[]> matchingPatterns, String toMatch)
+            throws TransformPatternTooLongException{
+        List<String> reverseToMatch = new ArrayList<>();
+        //split the string to match on spaces and commas but keep them in the list using the lookbehind pattern
+        //collect the string array into a linked list then reverse the order of the "sentence"
+        Stream.of(toMatch.split(SPLIT_ON_SPACE_LOOKBEHIND))
                 .collect(Collectors.toCollection(LinkedList::new))
                 .descendingIterator().forEachRemaining(reverseToMatch::add);
 
+        int patternLength;
 
+        attemptMatch:
+        for (String[] matchPattern : matchingPatterns) {
+            C1 c1 = new C1();
+            Pattern compiledMatchPattern = Pattern.compile(c1.createPatternForMatching(matchPattern[0]));
+            patternLength = c1.getPatternWordCount();
 
-        if(patternCount > 1) {
-            for(int i = 0; i<reverseToMatch.size()-patternCount;i++){
-                StringBuilder sb = new StringBuilder();
-                for(int j = patternCount; j>=0; j--) {
-                    //crawl the range through the list to cover all ordered-word possibilities
-                    sb.append(reverseToMatch.get(j+i));
+            if (patternLength <= reverseToMatch.size()) {
+                if (patternLength > 1) {
+                    for (int i = 0; i <= reverseToMatch.size() - patternLength; i++) {
+                        StringBuilder sb = new StringBuilder();
+                        for (int j = patternLength - 1; j >= 0; j--) {
+                            sb.append(reverseToMatch.get(i + j));
+                        }
+
+                        Matcher matcher = compiledMatchPattern.matcher(sb.toString());
+                        if (matcher.find()) {
+                            System.out.println("found match: " + matcher.group() + " using pattern: " + compiledMatchPattern.toString());
+
+                            List<String> transformPattern = c1.createPatternForTransforming(matchPattern[1]);
+                            if(c1.transformAvailable()) {
+                                transform(toMatch, matcher, transformPattern);
+                                break attemptMatch;
+                            }else
+                                throw new TransformPatternTooLongException("The transformation pattern must have a token count that is equal or less than the " +
+                                        "matching pattern.");
+                        }
+                    }
+
+                } else {
+                    for (int i = 0; i <= reverseToMatch.size() - 1; i++) {
+                        Matcher matcher = compiledMatchPattern.matcher(reverseToMatch.get(i));
+                        if (matcher.find()) {
+                            System.out.println("Original string: " + toMatch);
+                            System.out.println("found match: " + matcher.group() + " using pattern: " + compiledMatchPattern.toString());
+                            List<String> transformPattern = c1.createPatternForTransforming(matchPattern[1]);
+                            if (c1.transformAvailable()) {
+                                transform(toMatch, matcher, transformPattern);
+                                break attemptMatch;
+                            } else
+                                throw new TransformPatternTooLongException("The transformation pattern must be equal or smaller than the matching pattern.");
+                        }
+                    }
                 }
-
-                toMatch = sb.toString();
-                Matcher matcher = compiledPattern.matcher(toMatch);
-                if(matcher.find()) {
-                    System.out.println("found match: " + matcher.group());
-                    transform(matcher.group(), compiledTransformPatterns);
-                    break;
-                }
-                    //else System.out.println("no match found");
-            }
-
-        }else{
-            for(int i = 0; i<=reverseToMatch.size()-1;i++){
-                toMatch = reverseToMatch.get(i);
-
-                Matcher matcher = compiledPattern.matcher(toMatch);
-                if(matcher.find()){
-                    System.out.println("found match: " + matcher.group());
-                    break;
-                }
-                //else System.out.println("no match found");
             }
         }
     }
 
-    private static void transform(String toTranform, List<String> transformPattern){
+
+    private static void transform(String toMatch, Matcher matcher, List<String> transformPattern){
         StringBuilder sb = new StringBuilder();
-        String[] mappingTable = transformPattern.get(0).split(",");
-        String[] toMap = toTranform.split("");
 
-        for(int i = 0; i <=mappingTable.length-1;i++){
-            Pattern pattern = Pattern.compile(mappingTable[i]);
-            for(int j = 0; j<=toMap.length-1;j++){
-                Matcher matcher = pattern.matcher(toMap[j]);
-                if(matcher.find()){
-                    sb.append(toMap[j]);
-                    toMap[j] = "%&%";
-                    break;
-                }
-            }
+        System.out.println("Target Pattern " + transformPattern.toString());
+
+        transformPattern
+                .forEach(c -> sb.append(c.startsWith("~") ? c.split("~")[1] : matcher.group(c)));
+
+
+        System.out.println("Original String post-match: " + toMatch.replace(matcher.group(), ""));
+        System.out.println("Transformed Match: " + sb.toString());
+        System.out.println();
+    }
+
+   static class TransformPatternTooLongException extends Exception{
+        public TransformPatternTooLongException(String message){
+            super(message);
         }
-        System.out.println(sb.toString());
-
     }
 }
 
